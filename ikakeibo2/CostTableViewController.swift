f47a89976439c3412913b9c08d62cae070ef4687
Daigoh Hattori <ramenplease@gmail.com>
2017-04-15 20:50:57 +0900

    コスト画面 デザイン調整中

--- ikakeibo2/CostTableViewController.swift
+++ ikakeibo2/CostTableViewController.swift
@@ -74,6 +74,10 @@ class CostTableViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
+    }
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
