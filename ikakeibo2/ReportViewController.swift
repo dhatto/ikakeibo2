a2909625729693bd6c213cedd3eab5130a9de08b
Daigoh Hattori <ramenplease@gmail.com>
2017-07-29 09:58:18 +0900

    リファクタリング

--- ikakeibo2/ReportViewController.swift
+++ ikakeibo2/ReportViewController.swift
@@ -38,7 +38,38 @@ class ReportViewController: UIViewController, YearsSelectionDelegate {
     @IBAction func costTypeSegumentValueChanged(_ sender: UISegmentedControl) {
         totalizeCosts()
     }
+    
+    func setSummaryText(inComeTotal: Int, itemTotal: Int) {
+        let incomeValueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(inComeTotal))
+        let itemValueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(itemTotal))
+        
+        var itemLabelText = "収入：" + incomeValueWithFormat! + "\n" + "支出：" + itemValueWithFormat!
+        
+        if let totalValueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(inComeTotal - itemTotal)) {
+            itemLabelText = itemLabelText + "\n" + "収支：" + totalValueWithFormat
+        }
+        
+        // 0,1,3,4は使ってない
+        itemLabels[2].text = itemLabelText
+    }
+    
+    func setItemBest10(number: Int, key: String, itemTotal : Int, value: Int) {
 
+        // 下段に支出ベスト10を表示
+        if itemListLabels.count > number {
+            let per = Int(Double(value) / Double(itemTotal) * 100.0)
+            let valueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(value))
+            
+            var text = String(number + 1) + "." + key + "(" + String(per) + "%" + ")" + "\n"
+            text = text + valueWithFormat!
+
+            // インデント付きでテキストを設定
+            let indentStyle = paragpathTextStyle(indent: 20)
+            let attributedString = NSAttributedString(string: text, attributes: [NSParagraphStyleAttributeName: indentStyle])
+            itemListLabels[number].attributedText = attributedString
+        }
+    }
+    
     func totalizeCosts() {
         // 項目ごとの集計結果を取得
         let inComeTotal = RealmDataCenter.readTotalCost(
@@ -54,45 +85,23 @@ class ReportViewController: UIViewController, YearsSelectionDelegate {
         print(inComeTotal)
         print(itemTotal)
         
+        // 今月の、収入/支出/収支を表示
+        setSummaryText(inComeTotal: inComeTotal.sum, itemTotal: itemTotal.sum)
+
+        var i = 0
+
         // グラフパラメータを設定
         var params = [Dictionary<String,Float>]()
         var paramsItem = [Dictionary<String,String>]()
         var paramsColor = [Dictionary<String,UIColor>]()
 
-        var i = 0
-        let sum = inComeTotal.sum
-        let indentStyle = paragpathTextStyle(indent: 20)
-        
-        let incomeValueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(inComeTotal.sum))
-        let itemValueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(itemTotal.sum))
-        
-        var itemLabelText = "収入：" + incomeValueWithFormat! + "\n" + "支出：" + itemValueWithFormat!
-        
-        if let totalValueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(inComeTotal.sum - itemTotal.sum)) {
-            itemLabelText = itemLabelText + "\n" + "収支：" + totalValueWithFormat
-        }
-
-        itemLabels[2].text = itemLabelText
-
-        for (key,value) in (Array(itemTotal.dic).sorted {$0.1 > $1.1}) {
+        for (key, value) in (Array(itemTotal.dic).sorted {$0.1 > $1.1}) {
             params.append(["value": Float(value)])
             paramsItem.append(["item": key])
             paramsColor.append(["color": UIColor.randomColor()])
 
-            // 下段に支出ベスト10を表示
-            if itemListLabels.count > i {
-                let per = Int(Double(value) / Double(sum) * 100.0)
-                let valueWithFormat = DHLibrary.dhStringToString(withMoneyFormat: String(value))
-                
-                var text = String(i + 1) + "." + key + "(" + String(per) + "%" + ")" + "\n"
-                text = text + valueWithFormat!
-                
-                // インデント付きでテキストを設定
-                let attributedString = NSAttributedString(string: text, attributes: [NSParagraphStyleAttributeName: indentStyle])
-                itemListLabels[i].attributedText = attributedString
-
-                i = i + 1
-            }
+            setItemBest10(number: i, key: key, itemTotal: itemTotal.sum, value: value)
+            i = i + 1
         }
 
         // グラフ表示
@@ -100,7 +109,7 @@ class ReportViewController: UIViewController, YearsSelectionDelegate {
         graphView.setParams(params: params, paramsItem: paramsItem, paramsColor: paramsColor)
         graphView.startAnimating()
     }
-    
+
     func paragpathTextStyle(indent: CGFloat) -> NSMutableParagraphStyle {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.firstLineHeadIndent = indent
